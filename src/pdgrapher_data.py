db89aa75d7636445c080170b0450bf80f98239b3
joh-dah <johanna@dahlkemper.de>
2025-07-02 10:27:31 +0200

    run pdgrapher on example data

--- src/pdgrapher_data.py
+++ src/pdgrapher_data.py
@@ -1,10 +1,16 @@
-#%%
 # load data from external/PDGrapher/data/processed/torch_data/real_lognorm/data_forward_A549.pt
 import torch
 
 file_path = "external/PDGrapher/data/processed/torch_data/real_lognorm/data_forward_A549.pt"
 
-data = torch.load(file_path)
+data_forward = torch.load(file_path, weights_only=False)
 print(f"Data loaded successfully from {file_path}")
-data
-# %%
+# print length of data_forward
+print(f"Length of data_forward: {len(data_forward)}")
+
+file_path = "external/PDGrapher/data/processed/torch_data/real_lognorm/data_backward_A549.pt"
+data_backward = torch.load(file_path, weights_only=False)
+print(f"Data loaded successfully from {file_path}")
+# print length of data_backward
+print(f"Length of data_backward: {len(data_backward)}")
+data_backward
